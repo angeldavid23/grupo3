ec2f6f38cd89361c0c6c7889bd871a078cfd971e
leonardo <leonardotobe94@gmail.com>
2022-11-24 09:54:48 -0400

    basic fastapi

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 ## How to get started?
 The following command will install the packages according to the configuration file requirements.txt.
 
-```ruby
+```python
 $ pip install -r requirements.txt
+$ python3 app/main.py
 ```
--- app/main.py
+++ app/main.py
@@ -2,12 +2,19 @@ from typing import Union
 
 from fastapi import FastAPI
 
+import uvicorn
+
 app = FastAPI()
 
 @app.get("/")
 async def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "la"}
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+if __name__ == "__main__":
+    config = uvicorn.Config(app, port=8000)
+    server = uvicorn.Server(config)
+    server.run()
